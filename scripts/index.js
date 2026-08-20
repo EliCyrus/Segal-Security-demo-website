dadbdd3e9e325b4f4e0bd5a7b0ed582717e00938
EliCyrus <eli.cyrus00@gmail.com>
2026-08-20 15:20:36 -0400

    Updated animations

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,18 @@ const observer = new IntersectionObserver((entries)=> {
             else if ( entry.target.classList.contains('clients-interest-hidden')){
                  entry.target.classList.add('clients-interest-show')
             }
+            else if (entry.target.classList.contains('testimonial-left-hidden')){
+                entry.target.classList.add('testimonial-show')
+            }
+            else if (entry.target.classList.contains('testimonial-right-hidden')){
+                entry.target.classList.add('testimonial-show')
+            }
+            else if (entry.target.classList.contains('testimonial-hidden')){
+                entry.target.classList.add('testimonial-show')
+            }
+            else if (entry.target.classList.contains('message-hidden')){
+                entry.target.classList.add('message-show')
+            }
            
         }
 
@@ -27,7 +39,15 @@ const hiddenElements = document.querySelectorAll('.hidden')
 const servicesHiddenElements = document.querySelectorAll('.services-hidden')
 const clientsHiddenElements = document.querySelectorAll('.clients-hidden')
 const clientsInterestHiddenElements = document.querySelectorAll('.clients-interest-hidden')
+const testimonialLeftHidden = document.querySelectorAll('.testimonial-left-hidden')
+const testimonialrightHidden = document.querySelectorAll('.testimonial-right-hidden')
+const testimonialHidden = document.querySelectorAll('.testimonial-hidden')
+const messageHidden = document.querySelectorAll('.message-hidden')
 activateObserver(hiddenElements)
 activateObserver(servicesHiddenElements)
 activateObserver(clientsHiddenElements)
 activateObserver(clientsInterestHiddenElements)
+activateObserver(testimonialLeftHidden)
+activateObserver(testimonialrightHidden)
+activateObserver(testimonialHidden)
+activateObserver(messageHidden)
